df38c8b28bb6ad4836dfe18eb85f3499f55b2064
Lee Muro <lee@leemuro.com>
2011-06-14 23:03:24 -0400

    finished slides

--- enhance.js
+++ enhance.js
@@ -1,14 +1,5 @@
 $(function() {
 
-  // for demo only, to show effect on button for 5 seconds
-  $('form input[type=submit]:not([data-confirm])').click(function() {
-    setTimeout(function() { window.location = window.location; }, 5000);
-  });
-
-  // $('form input[type=submit]').click(function() {
-  //   $(this).val('Submitting...').attr('disabled', 'disabled');
-  // });
-
   $('[data-submit]').click(function() {
     var message = $(this).attr('data-submit');
     $(this).val(message).attr('disabled', 'disabled');
